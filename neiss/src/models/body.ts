a4044ea3948b41da677dc6062ebeead85f533839
FlorianNeiss <FlorianNeiss@gmail.com>
2018-12-12 11:02:08 +0100

    adding the models and changing the appComponent html

---
+++ neiss/src/models/body.ts
@@ -0,0 +1,19 @@
+import { Category } from './category';
+
+export class Body{
+    id: string;
+    title: string;
+    zip_code: string;
+    thumbnail: string;
+    attachments: string [];
+    counter: {
+        messages_total: number;
+        messages_unread: number;
+    };
+    is_awarded: boolean;
+    categories: Category[];
+    state: string;
+    description: string;
+    enddate: string;
+    created_at: string;
+}
---
+++ neiss/src/models/job.ts
@@ -0,0 +1,7 @@
+import { Body } from './body';
+
+export class Job {
+    errorno: number;
+    error: string;
+    body: Body;
+}
